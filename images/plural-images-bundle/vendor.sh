fcf9f370f294861320f5c3ea3d8fa4b3e1596231
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-10 16:46:43 +0100

    fix image vendor script

--- images/plural-images-bundle/vendor.sh
+++ images/plural-images-bundle/vendor.sh
@@ -3,13 +3,14 @@
 set -e
 
 IMAGES=(
-pluralsh/kairos-plural-bundle:0.1.4
+pluralsh/kairos-plural-cli:0.11.1
 pluralsh/deployment-operator:0.5.6
 pluralsh/agentk:0.0.2
 )
 
 for IMAGE in "${IMAGES[@]}"; do
-  ARCHIVE=${IMAGE//\//-}
+  VAR=${IMAGE//\//-}
+  ARCHIVE=${VAR//\:/-}
   echo "Copying $IMAGE to $ARCHIVE "
   skopeo copy docker://"$IMAGE" docker-archive:"$ARCHIVE".tar:"$IMAGE"
 done
