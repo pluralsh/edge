dfbf06b09a78f2c72af1a0a61c062d515a480722
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-13 16:25:05 +0100

    add script to prepare images with bundles included

---
+++ hack/prepare-image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "${IMAGE}" ]; then
+  echo "IMAGE environment variable not set"
+  exit 1
+fi
+
+echo "Exporting $IMAGE contents..."
+mkdir -p image
+docker create "$IMAGE" | xargs docker export > image.tar
+tar -xf image.tar -C image
+
+PLURAL_BUNDLE="ghcr.io/pluralsh/kairos-plural-bundle:0.1.4"
+echo "Adding $PLURAL_BUNDLE bundle..."
+mkdir -p image/run/initramfs/live/
+docker save $PLURAL_BUNDLE -o image/run/initramfs/live/plural-bundle.tar
+
+PLURAL_IMAGES_BUNDLE="ghcr.io/pluralsh/kairos-plural-images-bundle:0.1.0"
+echo "Adding $PLURAL_IMAGES_BUNDLE bundle..."
+mkdir -p image/run/initramfs/live/
+docker save $PLURAL_IMAGES_BUNDLE -o image/run/initramfs/live/plural-images-bundle.tar
