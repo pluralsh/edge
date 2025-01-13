012ae09d3fc9b0114eab2c220683db5af17f7f91
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-13 10:16:39 +0100

    use target arch

--- images/plural-images-bundle/vendor.sh
+++ images/plural-images-bundle/vendor.sh
@@ -2,16 +2,20 @@
 
 set -e
 
+TARGETARCH=$1
+
 IMAGES=(
 pluralsh/kairos-plural-cli:0.11.1
 pluralsh/deployment-operator:0.5.6
 pluralsh/agentk:0.0.2
 )
 
+echo "Using $TARGETARCH architecture"
+
 for IMAGE in "${IMAGES[@]}"; do
   VAR=${IMAGE//\//-}
   ARCHIVE=${VAR//\:/-}
   echo "Copying $IMAGE to $ARCHIVE..."
-  skopeo copy docker://"$IMAGE" docker-archive:"$ARCHIVE".tar:"$IMAGE"
+  skopeo copy --override-arch=$TARGETARCH docker://"$IMAGE" docker-archive:"$ARCHIVE".tar:"$IMAGE"
 done
 
