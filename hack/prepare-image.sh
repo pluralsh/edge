94d575e24f68d5cfb0dc7a1ce0457173d13c580b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-13 16:38:48 +0100

    update installation script

--- hack/prepare-image.sh
+++ hack/prepare-image.sh
@@ -15,9 +15,11 @@ tar -xf image.tar -C image
 PLURAL_BUNDLE="ghcr.io/pluralsh/kairos-plural-bundle:0.1.4"
 echo "Adding $PLURAL_BUNDLE bundle..."
 mkdir -p image/run/initramfs/live/
+docker pull $PLURAL_BUNDLE
 docker save $PLURAL_BUNDLE -o image/run/initramfs/live/plural-bundle.tar
 
 PLURAL_IMAGES_BUNDLE="ghcr.io/pluralsh/kairos-plural-images-bundle:0.1.0"
 echo "Adding $PLURAL_IMAGES_BUNDLE bundle..."
 mkdir -p image/run/initramfs/live/
+docker pull $PLURAL_IMAGES_BUNDLE
 docker save $PLURAL_IMAGES_BUNDLE -o image/run/initramfs/live/plural-images-bundle.tar
