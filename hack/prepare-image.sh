d33030b50227cbe89ea0f24824008c9e98e15110
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-14 13:27:47 +0100

    dockerize installation

--- hack/prepare-image.sh
+++ hack/prepare-image.sh
@@ -9,11 +9,10 @@ fi
 
 echo "Exporting $IMAGE contents..."
 mkdir -p image
-docker create "$IMAGE" | xargs docker export > image.tar
-tar -xf image.tar -C image
+luet util unpack "$IMAGE" /image
 
-echo "Preparing $BUNDLES_DIRECTORY..."
 BUNDLES_DIRECTORY=/image/usr/local/bundles
+echo "Preparing $BUNDLES_DIRECTORY..."
 mkdir -p $BUNDLES_DIRECTORY
 
 PLURAL_BUNDLE="ghcr.io/pluralsh/kairos-plural-bundle:0.1.4"
@@ -25,3 +24,6 @@ PLURAL_IMAGES_BUNDLE="ghcr.io/pluralsh/kairos-plural-images-bundle:0.1.1"
 echo "Adding $PLURAL_IMAGES_BUNDLE bundle..."
 docker pull $PLURAL_IMAGES_BUNDLE
 docker save $PLURAL_IMAGES_BUNDLE -o $BUNDLES_DIRECTORY/plural-images-bundle.tar
+
+echo "Building image..."
+/build-arm-image.sh  --model rpi4 --directory /image --config /cloud-config.yaml /tmp/build/kairos.img
