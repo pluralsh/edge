edaf552bbe945fb3c92ef2d93cce7e8346630488
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-21 14:57:46 +0100

    use edge bootstrap command

--- images/plural-bundle/run.sh
+++ images/plural-bundle/run.sh
@@ -4,14 +4,10 @@ set -ex
 
 K3S_MANIFEST_DIR=${K3S_MANIFEST_DIR:-/var/lib/rancher/k3s/server/manifests/}
 
-# DEFAULTS
 # renovate: datasource=docker depName=ghcr.io/pluralsh/plural-cli-cloud
-BASE_IMAGE="ghcr.io/pluralsh/kairos-plural-cli:0.11.1"
+BASE_IMAGE="ghcr.io/pluralsh/kairos-plural-cli:bootstrap" # TODO: Revert once https://github.com/pluralsh/plural-cli/pull/598 will be merged.
 TOKEN=""
 URL=""
-CLUSTER_NAME_PREFIX="plural-edge"
-PROJECT="default"
-TAG="plural=edge"
 
 getConfig() {
     local l=$1
@@ -44,21 +40,6 @@ readConfig() {
     if [ "$_url" != "" ]; then
         URL=$_url
     fi
-
-    _clusterNamePrefix=$(getConfig plural.clusterNamePrefix)
-    if [ "$_clusterNamePrefix" != "" ]; then
-        CLUSTER_NAME_PREFIX=$_clusterNamePrefix
-    fi
-
-    _project=$(getConfig plural.project)
-    if [ "$_project" != "" ]; then
-        PROJECT=$_project
-    fi
-
-    _tag=$(getConfig plural.tag)
-    if [ "$_tag" != "" ]; then
-        TAG=$_tag
-    fi
 }
 
 mkdir -p "${K3S_MANIFEST_DIR}"
@@ -70,9 +51,7 @@ for FILE in assets/*; do
   templ "BASE_IMAGE" "${BASE_IMAGE}" "${FILE}"
   templ "TOKEN" "${TOKEN}" "${FILE}"
   templ "URL" "${URL}" "${FILE}"
-  templ "CLUSTER_NAME" "${CLUSTER_NAME_PREFIX}-$(cut -c1-10 < /etc/plural-id)" "${FILE}"
-  templ "PROJECT" "${PROJECT}" "${FILE}"
-  templ "TAG" "${TAG}" "${FILE}"
+  templ "MACHINE_ID" "$(cat /etc/plural-id)" "${FILE}"
 done;
 
 cp -rfv assets/* "${K3S_MANIFEST_DIR}"
