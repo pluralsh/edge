030a756cd40fefbb40740649a2826c8fe113bb73
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-01-23 15:03:43 +0100

    add custom manager to update vendored images

--- images/plural-images-bundle/vendor.sh
+++ images/plural-images-bundle/vendor.sh
@@ -4,11 +4,14 @@ set -e
 
 TARGETARCH=$1
 
-# TODO: Write renovate config to keep it up to date.
 IMAGES=(
+# renovate: datasource=docker packageName=ghcr.io/pluralsh/kairos-plural-cli versioning=docker
 ghcr.io/pluralsh/kairos-plural-cli:0.11.1
+# renovate: datasource=docker packageName=ghcr.io/pluralsh/deployment-operator versioning=docker
 ghcr.io/pluralsh/deployment-operator:0.5.6
+# renovate: datasource=docker packageName=ghcr.io/pluralsh/agentk versioning=docker
 ghcr.io/pluralsh/agentk:0.0.2
+# renovate: datasource=docker packageName=alpine/curl versioning=docker
 alpine/curl:8.10.0
 )
 
